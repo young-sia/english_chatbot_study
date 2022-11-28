62d0ae3e8e120d8a57b8d70101cb709e00cf62e0
young-sia <youngly1028@gmail.com>
2022-11-28 15:33:19 +0900

    updated website design

--- app.py
+++ app.py
@@ -12,19 +12,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/manual')
-def pick_page():
-    return render_template('first_chatbot_pick_chatbot.html')
+# @app.route('/manual')
+# def pick_page():
+#     return render_template('first_chatbot_pick_chatbot.html')
 
 
 @app.route('/chatbot1')
 def chatbot_page1():
-    return render_template('first_chatbot_bot1.html', image_file='images/apple.jpg')
+    return render_template('first_chatbot_bot1.html')
 
 
 @app.route('/chatbot2')
 def chatbot_page2():
     return render_template('first_chatbot_bot2.html')
+
+
+@app.route('/chatbot3')
+def chatbot_page3():
+    return render_template('first_chatbot_bot3.html')
 
 
 # run Flask app
